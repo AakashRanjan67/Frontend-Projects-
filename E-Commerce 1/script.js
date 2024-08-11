6756b58d7dfdc5431bc523e292bfaa694b99b8b8
Aakash Ranjan <aakashranjan67@gmail.com>
2024-08-11 22:25:31 +0530

    Add files via upload

---
+++ E-Commerce 1/script.js
@@ -0,0 +1,17 @@
+// Script for navigation bar
+
+const bar = getElementById("bar");
+const close = getElementById("close");
+const nav = getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
